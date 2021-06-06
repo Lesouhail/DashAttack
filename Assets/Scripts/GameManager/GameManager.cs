d76b7d8c2ae18099b1adba4814aae9c08f3bf5c0
Sylvain Delépée <sylvain.delepee@gmail.com>
2021-06-06 16:03:37 +0200

    lock framerate at 60fps

---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,16 @@
+﻿namespace DashAttack.GameManager
+{
+    using UnityEngine;
+
+    public class GameManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Application.targetFrameRate = 60;
+        }
+
+        public void OnApplicationQuit()
+        {
+        }
+    }
+}
